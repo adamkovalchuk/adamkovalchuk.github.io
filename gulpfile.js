6a5bf5a074ef8ba6b974d889f82ae64faf5aca33
adamkoval <adamkovalchuk18@gmail.com>
2021-06-13 19:58:23 +0300

    Add consultation page

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var concat = require('gulp-concat')
 var minifyCSS = require('gulp-minify-css');
-var path = require('path')
-var CWD = process.cwd()
-
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
@@ -17,5 +13,3 @@ gulp.task('sass', function () {
 gulp.task('watch', function() {
     gulp.watch('./sass/**/*.sass', gulp.series('sass'));
 });
-
-gulp.task('default', gulp.series('watch'))
