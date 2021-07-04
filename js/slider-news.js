a33ddbf1bb78c53fe720af9ca3554e511c10b89f
adamkoval <adamkovalchuk18@gmail.com>
2021-07-04 16:32:08 +0300

    added news slider

---
+++ js/slider-news.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('.news-boxes-container').slick({
+      prevArrow: '.news-slider__controls-prev',
+      nextArrow: '.news-slider__controls-next',
+      slidesToShow: 3,
+      touchThreshold: Infinity,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      pauseOnHover: true,
+      slidesToScroll: 1,
+      centerMode: true,
+      respondTo: 'window',
+      
+      responsive: [
+        {
+          breakpoint: 1399,
+          settings: {
+            slidesToShow: 3,
+            centerMode: false,
+            autoplaySpeed: 2000
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            swipe: true,  
+            swipeToSlide: true,        
+          }
+        },
+      ]
+    })
+  });
--- js/slider.js
+++ js/slider.js
@@ -1,32 +1,33 @@
 const slideCurrent = document.getElementsByClassName('slide-current')[0]
 
 $(document).ready(function(){
-    $('.team-slider').slick({
-        prevArrow: '.team-slider__controls-prev',
-        nextArrow: '.team-slider__controls-next',
-        slidesToShow: 4,
-        touchThreshold: Infinity,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        pauseOnHover: true,
-        slidesToScroll: 1,
-        centerMode: true,
-        respondTo: 'window',
-        responsive: [
-            {
-              breakpoint: 1399,
-              settings: {
-                slidesToShow: 3,
-              }
-            },
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 2,
-              }
-            },
-        ]
-    })
-  });
+  $('.team-slider').slick({
+    prevArrow: '.team-slider__controls-prev',
+    nextArrow: '.team-slider__controls-next',
+    slidesToShow: 4,
+    touchThreshold: Infinity,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    pauseOnHover: true,
+    slidesToScroll: 1,
+    centerMode: false,
+    respondTo: 'window',
+    
+    responsive: [
+      {
+        breakpoint: 1399,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+    ]
+  })
+});
 
 
